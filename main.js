8f2d0fa7feeb739dcb698eca686d9664ccba285b
Aldo Santos <aldoacesso@gmail.com>
2023-05-22 14:09:25 -0300

    add commit

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    });
+
+    $('#botao-finalizar').click(function() {
+        $('form').slideUp();
+    });
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const tarefa = $('#tarefa').val();
+        /*console.log(tarefa);*/
+
+        
+        const novaTarefa = $(`<li class="ticar">${tarefa}</li>`);
+        $(`${tarefa}`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        $('#tarefa').val('');
+
+        $('ul').on('click','li',function(){
+            /*alert($(this).text());*/
+            $('.ticar').css('text-decoration', 'line-through');
+        });
+    });
+
+
+    /*
+    
+    $('ul').on('mouseenter', function(){
+        $('li').css('text-decoration', 'line-through');
+    });
+    $('ul').on('mouseleave', function(){
+        $('li').css('text-decoration', 'none');
+    });
+    */
+});
